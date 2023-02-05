973f12bbdc33f273d91d080eb3f2c64987463dde
NataliaAndenko <120939798+NataliaAndenko@users.noreply.github.com>
2023-02-05 13:57:57 +0300

    Update Program.cs

--- 003/Program.cs
+++ 003/Program.cs
@@ -7,27 +7,60 @@
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
 
 
-Random ran = new Random();
-int[,] arr = new int[ran.Next(1, 10), ran.Next(1, 10)];
-for (int i = 0; i < arr.GetLength(0); i++)
+int rows = 3;
+int colums = 4;
+int[,] numbers = new int[rows, colums];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+double[] avgNumbers = new double[numbers.GetLength(1)];
+
+for (int i = 0; i < numbers.GetLength(1); i++)
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    double result = 0.0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
     {
-        arr[i, j] = ran.Next(1, 10);
-        Console.Write(arr[i, j] + " ");
+        result += numbers[j, i];
+    }
+    avgNumbers[i] = result / numbers.GetLength(0);
+}
+PrintArray(avgNumbers);
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
     }
     Console.WriteLine();
 }
-Console.WriteLine("---------------------------");
-Console.WriteLine(arr.GetLength(0));
-for (int j = 0; j < arr.GetLength(1); j++)
+
+void PrintArray(double[] array)
 {
-    double sum = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        sum += arr[i, j];
+        Console.Write(array[i] + " ");
     }
-    for (int i = 0; i < arr.GetLength(0); i++)
-    Console.Write($"{ sum / arr.GetLength(0)} ");
+    Console.WriteLine();
 }
-Console.ReadLine();
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
