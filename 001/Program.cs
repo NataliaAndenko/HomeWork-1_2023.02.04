80eaa766e9529db1fd3b2b0075de2ab14c533fdd
Natalia <zhuravlevanyu@mail.ru>
2023-02-05 11:43:17 +0300

    gitign

---
+++ 001/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateMatrix()
+{
+    double[,] matrix = new double[3, 4];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.NextDouble() * 10;
+        }
+    }
+    return matrix;
+}
+
+double[,] matrix = CreateMatrix();
+PrintArray(matrix);
